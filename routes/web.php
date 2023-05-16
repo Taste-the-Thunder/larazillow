2ccc7a6ae77d63458558643f81e0622717d5e671
dhruvinprajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2023-05-16 14:54:41 +0530

    auth and login setup

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[IndexController::class, 'index']);
 Route::get('/show',[IndexController::class, 'show']);
 Route::resource('/listing', ListingController::class);
+
+Route::get('login', [AuthController::class, 'create'])->name('login');
+Route::post('login', [AuthController::class, 'store'])->name('login.store');
+Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
