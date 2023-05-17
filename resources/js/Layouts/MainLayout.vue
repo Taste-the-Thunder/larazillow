e36e2f6deffbd47c670b4f660cd363091b949e9c
dhruvinprajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2023-05-17 11:00:07 +0530

    show user name & logout setup

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -8,8 +8,15 @@
         <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
           <Link :href="route('listing.index')">LaraZillow</Link>
         </div>
-        <div>
+        <div v-if="user" class="flex items-center gap-4">
+          <div class="text-sm text-gray-500">{{ user.name }}</div>
           <Link :href="route('listing.create')" class="btn-primary">+ New Listing</Link>
+          <div>
+            <Link :href="route('logout')" method="delete" as="button">Logout</Link>
+          </div>
+        </div>
+        <div v-else>
+          <Link :href="route('login')">Sign-In</Link>
         </div>
       </nav>
     </div>
@@ -29,5 +36,9 @@ import { computed } from 'vue'
 
 const flashSuccess = computed(
   () => usePage().props.flash.success,
+)
+
+const user = computed(
+  () => usePage().props.user,
 )
 </script>
