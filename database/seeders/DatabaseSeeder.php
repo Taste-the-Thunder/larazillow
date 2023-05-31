2b054d83e4fded0949138d4e54174cedb3bdf761
Dhruvin Prajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2023-05-31 10:54:15 +0530

    factory password change

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Dhruvin Prajapati',
+            'email' => 'dj@gmail.com',
         ]);
 
         Listing::factory(20)->create();
