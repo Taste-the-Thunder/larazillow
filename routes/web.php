e0f9b0b8c7b294c3b758403bed03b6219cf9f31f
Dhruvin Prajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2023-06-14 15:48:47 +0530

    realtor index

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\UserAccountController;
 
 /*
@@ -26,3 +27,11 @@ Route::post('login', [AuthController::class, 'store'])->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
 
 Route::resource('user-account', UserAccountController::class)->only(['create', 'store']);
+
+
+Route::prefix('realtor')
+  ->name('realtor.')
+  ->middleware('auth')
+  ->group(function () {
+    Route::resource('listing', RealtorListingController::class);
+  });
